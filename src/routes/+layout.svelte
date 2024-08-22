016cd69e96b5a182ba7c58ddf986ff3f6115b177
Øyvind Øyen <oyvindo@gmail.com>
2024-08-22 13:57:05 +0200

    Add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css'
+
+	$effect(() => {
+		console.log('+layout.svelte')
+	})
+	let { children } = $props()
+</script>
+
+<div class="app">
+	<main>
+		{@render children()}
+	</main>
+</div>
